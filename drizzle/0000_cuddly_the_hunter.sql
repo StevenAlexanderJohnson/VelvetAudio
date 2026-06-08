d1f30ddfb28c7bd0b85e8e42d1b78e748dd3d4ec
StevenAlexanderJohnson <56038965+StevenAlexanderJohnson@users.noreply.github.com>
2026-06-07 21:55:12 -0400

    nuking migrations becuase honestly nobody is using them and I'm trying to get docker set up.

--- drizzle/0000_cuddly_the_hunter.sql
+++ drizzle/0000_cuddly_the_hunter.sql
@@ -3,9 +3,11 @@ CREATE TABLE `episodes` (
 	`podcast_id` integer,
 	`guid` text NOT NULL,
 	`title` text NOT NULL,
+	`image` text,
 	`audio_url` text NOT NULL,
 	`pub_date` text NOT NULL,
-	`downloaded_date` integer DEFAULT '"2026-05-09T13:37:39.907Z"' NOT NULL,
+	`downloaded_date` integer,
+	`exempt_cleanup` integer DEFAULT false,
 	FOREIGN KEY (`podcast_id`) REFERENCES `podcasts`(`id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
@@ -15,13 +17,36 @@ CREATE TABLE `podcasts` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`name` text NOT NULL,
 	`rss_url` text NOT NULL,
+	`image` text,
 	`next_run_at` integer NOT NULL,
 	`max_downloaded` integer NOT NULL
 );
 --> statement-breakpoint
 CREATE INDEX `next_run_idx` ON `podcasts` (`next_run_at`);--> statement-breakpoint
+CREATE TABLE `sessions` (
+	`id` text PRIMARY KEY NOT NULL,
+	`user_id` text NOT NULL,
+	`expires_at` integer NOT NULL,
+	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE no action
+);
+--> statement-breakpoint
+CREATE TABLE `settings` (
+	`key` text PRIMARY KEY NOT NULL,
+	`value` text NOT NULL
+);
+--> statement-breakpoint
 CREATE TABLE `task` (
 	`id` text PRIMARY KEY NOT NULL,
 	`title` text NOT NULL,
 	`priority` integer DEFAULT 1 NOT NULL
 );
+--> statement-breakpoint
+CREATE TABLE `users` (
+	`id` text PRIMARY KEY NOT NULL,
+	`username` text NOT NULL,
+	`email` text NOT NULL,
+	`password_hash` text NOT NULL
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX `users_username_unique` ON `users` (`username`);--> statement-breakpoint
+CREATE UNIQUE INDEX `users_email_unique` ON `users` (`email`);
